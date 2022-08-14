9a2550870590f8aedd65cd5a94fea0810ca7952b
Ohad <ohad.d@zigit.co.il>
2022-08-14 19:21:16 +0300

    modified ExplanationsPage

--- client/src/pages/ExplanationsPage/ExplanationsPage.style.ts
+++ client/src/pages/ExplanationsPage/ExplanationsPage.style.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { StyledButton } from '../../components/Button/Button.style';
 
 export const ExplanationsPageContainer = styled.div`
     display: flex;
@@ -8,5 +9,12 @@ export const ExplanationsPageContainer = styled.div`
     font-size:2rem;
 `
 
-
-
+export const ExplanationsPageButton = styled(StyledButton)`
+    justify-content: center;
+    margin: 20px auto;;
+    color: ${(props) => props.theme.colors.Gold};
+    background:linear-gradient(72.2deg,#39454c 0%,#64757e 100%);
+    box-shadow: 1px 1px 10px blue;
+    font-size:2rem;
+    font-weight:bold;
+`
--- client/src/pages/ExplanationsPage/ExplanationsPage.tsx
+++ client/src/pages/ExplanationsPage/ExplanationsPage.tsx
@@ -1,12 +1,11 @@
 import { useNavigate } from 'react-router-dom';
 import * as ROUTES from '../../constants/routes';
-import { ExplanationsPageContainer } from './ExplanationsPage.style';
+import { ExplanationsPageContainer, ExplanationsPageButton } from './ExplanationsPage.style';
 
 const ExplanationsPage = () => {
   const navigate = useNavigate();
   return <ExplanationsPageContainer>Learn How To Add Reviews Page
-    <button onClick={() => navigate(ROUTES.HOME)}>Go Back To Home Page</button>
-
+    <ExplanationsPageButton onClick={() => navigate(ROUTES.HOME)}>Go Back To Home Page</ExplanationsPageButton>
   </ExplanationsPageContainer>
 };
 
